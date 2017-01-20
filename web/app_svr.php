d13f9971e44013fdfcc1808b85d6195c6c7feb85
Jefry, Bebeto <Bebeto.Jefry@nyumc.org>
2017-01-20 15:35:02 +0530

    SVR env fix

--- web/app_svr.php
+++ web/app_svr.php
@@ -3,8 +3,10 @@
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Debug\Debug;
 
-$loader = require __DIR__.'/../app/autoload.php';
+$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 Debug::enable();
+
+require_once __DIR__.'/../app/AppKernel.php';
 
 $kernel = new AppKernel('svr', true);
 $kernel->loadClassCache();
