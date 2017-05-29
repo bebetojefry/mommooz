9a7ee6f3e711e6ef8ebb63584d6cd62d65d50cfe
bebeto <bebeto@qburst.com>
2017-05-29 19:13:55 +0530

    datatable caching

--- src/App/FrontBundle/Datatables/ItemDatatable.php
+++ src/App/FrontBundle/Datatables/ItemDatatable.php
@@ -68,7 +68,7 @@ class ItemDatatable extends AbstractDatatableView
             'scroll_x' => false,
             'scroll_y' => '',
             'searching' => true,
-            'state_save' => false,
+            'state_save' => true,
             'delay' => 0,
             'extensions' => array(),
             'highlight' => false,
