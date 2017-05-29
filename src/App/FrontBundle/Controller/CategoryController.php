cf581b04c59bc2a50d25f3933ce65638dbdeab8a
bebeto <bebeto@qburst.com>
2017-05-29 19:41:39 +0530

    datatable caching

--- src/App/FrontBundle/Controller/CategoryController.php
+++ src/App/FrontBundle/Controller/CategoryController.php
@@ -7,6 +7,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use App\FrontBundle\Entity\Category;
 use App\FrontBundle\Form\CategoryType;
 use App\FrontBundle\Helper\FormHelper;
