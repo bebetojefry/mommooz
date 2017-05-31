b223077ca51824255bc18e51e69f0f3abd120242
bebeto <bebeto@qburst.com>
2017-05-31 19:06:22 +0530

    vendor item datatable translations

--- src/App/FrontBundle/Controller/VendorItemController.php
+++ src/App/FrontBundle/Controller/VendorItemController.php
@@ -56,8 +56,8 @@ class VendorItemController extends Controller
     {
         $dm = $this->getDoctrine()->getManager();
         
-        if(1){
-            $category = $dm->getRepository('AppFrontBundle:Category')->find(1);
+        if($request->isMethod('POST')){
+            $category = $dm->getRepository('AppFrontBundle:Category')->find($_POST['vendor_opt_category']);
             if($category){
                 $body = $this->renderView('AppFrontBundle:Vendor:cat_items.html.twig',
                     array('category' => $category, 'vendor' => $vendor)
